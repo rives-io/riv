9488b288ee23bcc445d5a5a54fcde57ff201ed04
Eduardo Bart <edub4rt@gmail.com>
2023-08-05 21:15:32 -0300

    C API

--- libriv/riv.h
+++ libriv/riv.h
@@ -1,12 +1,20 @@
-## pragmas.unitname = ''
+#ifndef RIV_H
+#define RIV_H
 
---------------------------------------------------------------------------------
--- Enums
+#include <stdint.h>
+#include <stdbool.h>
 
--- All supported RIV key codes, specially mapped to fit in 7 bits.
-global riv_key_code: type <using> = @enum(uint8) {
+////////////////////////////////////////////////////////////////////////////////
+// Defines
+
+#define RIV_VERSION 1000
+
+////////////////////////////////////////////////////////////////////////////////
+// Constants
+
+typedef enum riv_key_code {
   RIV_KEYCODE_INVALID             = 0,
-  -- F keys
+  // F keys
   RIV_KEYCODE_F1                  = 1,
   RIV_KEYCODE_F2                  = 2,
   RIV_KEYCODE_F3                  = 3,
@@ -19,7 +27,7 @@ global riv_key_code: type <using> = @enum(uint8) {
   RIV_KEYCODE_F10                 = 10,
   RIV_KEYCODE_F11                 = 11,
   RIV_KEYCODE_F12                 = 12,
-  -- numeric keys
+  // numeric keys
   RIV_KEYCODE_BACKSPACE           = 13,
   RIV_KEYCODE_NUM_LOCK            = 14,
   RIV_KEYCODE_KP_ENTER            = 15,
@@ -39,23 +47,23 @@ global riv_key_code: type <using> = @enum(uint8) {
   RIV_KEYCODE_KP_SUBTRACT         = 29,
   RIV_KEYCODE_KP_ADD              = 30,
   RIV_KEYCODE_KP_EQUAL            = 31,
-  -- printable keys
+  // printable keys
   RIV_KEYCODE_SPACE               = 32,
-  -- ASCII 33 !
-  -- ASCII 34 "
-  -- ASCII 35 #
-  -- ASCII 36 $
-  -- ASCII 37 %
-  -- ASCII 38 &
-  RIV_KEYCODE_APOSTROPHE          = 39,  -- '
-  -- ASCII 40 (
-  -- ASCII 41 )
-  -- ASCII 42 *
-  -- ASCII 43 +
-  RIV_KEYCODE_COMMA               = 44,  -- ,
-  RIV_KEYCODE_MINUS               = 45,  -- -
-  RIV_KEYCODE_PERIOD              = 46,  -- .
-  RIV_KEYCODE_SLASH               = 47,  -- /
+  // ASCII 33 !
+  // ASCII 34 "
+  // ASCII 35 #
+  // ASCII 36 $
+  // ASCII 37 %
+  // ASCII 38 &
+  RIV_KEYCODE_APOSTROPHE          = 39,  // '
+  // ASCII 40 (
+  // ASCII 41 )
+  // ASCII 42 *
+  // ASCII 43 +
+  RIV_KEYCODE_COMMA               = 44,  // ,
+  RIV_KEYCODE_MINUS               = 45,  // -
+  RIV_KEYCODE_PERIOD              = 46,  // .
+  RIV_KEYCODE_SLASH               = 47,  // /
   RIV_KEYCODE_0                   = 48,
   RIV_KEYCODE_1                   = 49,
   RIV_KEYCODE_2                   = 50,
@@ -66,13 +74,13 @@ global riv_key_code: type <using> = @enum(uint8) {
   RIV_KEYCODE_7                   = 55,
   RIV_KEYCODE_8                   = 56,
   RIV_KEYCODE_9                   = 57,
-  -- ASCII 58 :
-  RIV_KEYCODE_SEMICOLON           = 59,  -- ;
-  -- ASCII 60 <
-  RIV_KEYCODE_EQUAL               = 61,  -- =
-  -- ASCII 62 >
-  -- ASCII 63 ?
-  -- ASCII 64 @
+  // ASCII 58 :
+  RIV_KEYCODE_SEMICOLON           = 59,  // ;
+  // ASCII 60 <
+  RIV_KEYCODE_EQUAL               = 61,  // =
+  // ASCII 62 >
+  // ASCII 63 ?
+  // ASCII 64 @
   RIV_KEYCODE_A                   = 65,
   RIV_KEYCODE_B                   = 66,
   RIV_KEYCODE_C                   = 67,
@@ -99,13 +107,13 @@ global riv_key_code: type <using> = @enum(uint8) {
   RIV_KEYCODE_X                   = 88,
   RIV_KEYCODE_Y                   = 89,
   RIV_KEYCODE_Z                   = 90,
-  RIV_KEYCODE_LEFT_BRACKET        = 91,  -- [
-  RIV_KEYCODE_BACKSLASH           = 92,  -- \
-  RIV_KEYCODE_RIGHT_BRACKET       = 93,  -- ]
-  -- ASCII 94 ^
-  -- ASCII 95 _
-  RIV_KEYCODE_GRAVE_ACCENT        = 96,  -- `
-  -- other keys
+  RIV_KEYCODE_LEFT_BRACKET        = 91,  // [
+  RIV_KEYCODE_BACKSLASH           = 92,  // '\'
+  RIV_KEYCODE_RIGHT_BRACKET       = 93,  // ]
+  // ASCII 94 ^
+  // ASCII 95 _
+  RIV_KEYCODE_GRAVE_ACCENT        = 96,  // `
+  // other keys
   RIV_KEYCODE_ESCAPE              = 97,
   RIV_KEYCODE_ENTER               = 98,
   RIV_KEYCODE_TAB                 = 99,
@@ -132,13 +140,13 @@ global riv_key_code: type <using> = @enum(uint8) {
   RIV_KEYCODE_RIGHT_ALT           = 120,
   RIV_KEYCODE_RIGHT_SUPER         = 121,
   RIV_KEYCODE_MENU                = 122,
-  -- ASCII 123 {
-  -- ASCII 124 |
-  -- ASCII 125 }
-  -- ASCII 126 ~
-}
+  // ASCII 123 {
+  // ASCII 124 |
+  // ASCII 125 }
+  // ASCII 126 ~
+} riv_key_code;
 
-global riv_pal16_color: type <using> = @enum(uint8) {
+typedef enum riv_pal16_color {
   RIV_PAL16_BLACK      = 0,
   RIV_PAL16_DARKBLUE   = 1,
   RIV_PAL16_DARKPURPLE = 2,
@@ -155,9 +163,9 @@ global riv_pal16_color: type <using> = @enum(uint8) {
   RIV_PAL16_LAVENDER   = 13,
   RIV_PAL16_PINK       = 14,
   RIV_PAL16_LIGHTPEACH = 15,
-}
+} riv_pal16_color;
 
-global riv_rgb_pal16_color: type <using> = @enum(uint32) {
+typedef enum riv_rgb_pal16_color {
   RIV_RGB_PAL16_BLACK      = 0x000000,
   RIV_RGB_PAL16_DARKBLUE   = 0x532B1D,
   RIV_RGB_PAL16_DARKPURPLE = 0x53257E,
@@ -174,200 +182,189 @@ global riv_rgb_pal16_color: type <using> = @enum(uint32) {
   RIV_RGB_PAL16_LAVENDER   = 0x9C7683,
   RIV_RGB_PAL16_PINK       = 0xA877FF,
   RIV_RGB_PAL16_LIGHTPEACH = 0xAACCFF,
-}
+} riv_rgb_pal16_color;
 
-global riv_control_reason: type <using> = @enum(uint32) {
+typedef enum riv_control_reason {
   RIV_CONTROL_UNKNOWN = 0,
   RIV_CONTROL_PRESENT,
   RIV_CONTROL_AUDIO,
-}
+} riv_control_reason;
 
-global riv_pixel_format: type <using> = @enum(uint32) {
+typedef enum riv_pixel_format {
   RIV_PIXELFORMAT_UNKNOWN = 0,
   RIV_PIXELFORMAT_PAL256,
-  -- RIV_PIXELFORMAT_RGB8,
-  -- RIV_PIXELFORMAT_RGB16,
-  -- RIV_PIXELFORMAT_RGB24,
-  -- RIV_PIXELFORMAT_RGB32,
-  -- RIV_PIXELFORMAT_RGB32F,
-}
+  // RIV_PIXELFORMAT_RGB8,
+  // RIV_PIXELFORMAT_RGB16,
+  // RIV_PIXELFORMAT_RGB24,
+  // RIV_PIXELFORMAT_RGB32,
+  // RIV_PIXELFORMAT_RGB32F,
+  RIV_PIXELFORMAT_DEFAULT = RIV_PIXELFORMAT_PAL256
+} riv_pixel_format;
 
-global riv_audio_command: type <using> = @enum(uint32) {
+typedef enum riv_audio_command {
   RIV_AUDIOCOMMAND_NONE = 0,
   RIV_AUDIOCOMMAND_SOUND_PLAY,
   RIV_AUDIOCOMMAND_SOUND_STOP,
-}
+} riv_audio_command;
 
-global riv_mem_size: type <using> = @enum(uint64) {
-  RIV_MEMSIZE_HUGEPAGE     =   2*1024*1024, -- 2 MB
-  RIV_MEMSIZE_CARTRIDGE    =  16*1024*1024, -- 16 MB
-  RIV_MEMSIZE_RAM          = 128*1024*1024, -- 128 MB
-  -- driver
-  RIV_MEMSIZE_MMIO_DRIVER  =  128*1024, -- 128 KB
-  RIV_MEMSIZE_MMIO_DEVICE  =  128*1024, -- 128 KB
-  RIV_MEMSIZE_AUDIOBUFFER  =  768*1024, -- 768 KB
-  RIV_MEMSIZE_FRAMEBUFFER  = 1024*1024, -- 1 MB
-}
+typedef enum riv_mem_size {
+  RIV_MEMSIZE_HUGEPAGE     =   2*1024*1024, // 2 MB
+  RIV_MEMSIZE_CARTRIDGE    =  16*1024*1024, // 16 MB
+  RIV_MEMSIZE_RAM          = 128*1024*1024, // 128 MB
+  // driver
+  RIV_MEMSIZE_MMIO_DRIVER  =  128*1024, // 128 KB
+  RIV_MEMSIZE_MMIO_DEVICE  =  128*1024, // 128 KB
+  RIV_MEMSIZE_AUDIOBUFFER  =  768*1024, // 768 KB
+  RIV_MEMSIZE_FRAMEBUFFER  = 1024*1024, // 1 MB
+} riv_mem_size;
 
-global riv_mmio_offset: type <using> = @enum(uint64) {
+typedef enum riv_mmio_offset {
   RIV_MMIOSTART_MMIO_DRIVER  = 0,
   RIV_MMIOSTART_MMIO_DEVICE  = RIV_MEMSIZE_MMIO_DRIVER,
   RIV_MMIOSTART_AUDIOBUFFER  = RIV_MEMSIZE_MMIO_DRIVER + RIV_MEMSIZE_MMIO_DEVICE,
   RIV_MMIOSTART_FRAMEBUFFER  = RIV_MEMSIZE_MMIO_DRIVER + RIV_MEMSIZE_MMIO_DEVICE + RIV_MEMSIZE_AUDIOBUFFER,
-}
+} riv_mmio_offset;
 
-global RIV_VADDR_BASE: uint64 <comptime> = 0x10000000
-
-global riv_vaddr_base: type <using> = @enum(uint64) {
+typedef enum riv_vaddr_base {
+  RIV_VADDR_BASE         = 0x10000000,
   RIV_VADDR_MMIO_DRIVER  = RIV_VADDR_BASE + RIV_MMIOSTART_MMIO_DRIVER,
   RIV_VADDR_MMIO_DEVICE  = RIV_VADDR_BASE + RIV_MMIOSTART_MMIO_DEVICE,
   RIV_VADDR_AUDIOBUFFER  = RIV_VADDR_BASE + RIV_MMIOSTART_AUDIOBUFFER,
   RIV_VADDR_FRAMEBUFFER  = RIV_VADDR_BASE + RIV_MMIOSTART_FRAMEBUFFER,
-}
+} riv_vaddr_base;
 
-global riv_paddr_base: type <using> = @enum(uint64) {
+typedef enum riv_paddr_base {
   RIV_PADDR_RAM        = 0x80000000,
   RIV_PADDR_ROOTFS     = 0x80000000000000,
   RIV_PADDR_CARTRIDGE  = 0x90000000000000,
-}
+} riv_paddr_base;
 
-global riv_fps_limit: type <using> = @enum(uint32) {
+typedef enum riv_fps_limit {
   RIV_FPS_INVALID = 0,
   RIV_FPS_24 = 24,
   RIV_FPS_30 = 30,
   RIV_FPS_48 = 48,
   RIV_FPS_60 = 60,
   RIV_FPS_UNLIMITED = 0xffffffff,
-}
+} riv_fps_limit;
 
-global riv_constants: type <using> = @enum(uint32) {
-  RIV_MAX_CYCLESPERSECOND = 30*1000*1000, -- 30 MHz
-  RIV_VERSION = 000001000,
+typedef enum riv_constants {
   RIV_DEFAULT_WIDTH = 256,
   RIV_DEFAULT_HEIGHT = 256,
   RIV_DEFAULT_FPS = 60,
-  RIV_DEFAULT_PIXELFORMAT = RIV_PIXELFORMAT_PAL256,
-}
+} riv_constants;
 
---------------------------------------------------------------------------------
--- Constants
+typedef struct riv_span_uint8 {
+  uint8_t* data;
+  uintptr_t size;
+} riv_span_uint8;
 
-global RIV_DRIVER_MAGIC: [32]byte <const> = {
-  0x3f, 0xdf, 0x37, 0x1e, 0xc0, 0xfc, 0xd1, 0xba,
-  0xec, 0xe9, 0x72, 0xa1, 0xf5, 0x89, 0x87, 0xc5,
-  0x70, 0xfd, 0xbe, 0xc0, 0xce, 0xcc, 0x2d, 0x74,
-  0x8d, 0x45, 0x39, 0x62, 0x49, 0xb8, 0x15, 0x26
-}
+////////////////////////////////////////////////////////////////////////////////
+// Structures used by both device and driver
 
-global RIV_DEVICE_MAGIC: [32]byte <const> = {
-  0x83, 0x0b, 0x3a, 0xd1, 0xcc, 0x8b, 0xc2, 0xe5,
-  0x70, 0x5c, 0x83, 0x98, 0x6c, 0xe4, 0x67, 0xc9,
-  0xc1, 0xc6, 0x0b, 0xc6, 0xb9, 0x80, 0xa4, 0x1c,
-  0x34, 0x12, 0x8c, 0x2e, 0x05, 0xd8, 0x2c, 0x4e
-}
+typedef struct riv_framebuffer_desc {
+  uint32_t height;
+  uint32_t width;
+  uint32_t fps;
+  riv_pixel_format pixel_format;
+} riv_framebuffer_desc;
 
---------------------------------------------------------------------------------
--- Structures for both device and driver
+typedef struct riv_audio_ctl_desc {
+  riv_audio_command command;
+  uint32_t flags;
+  uint64_t handle_id;
+  uint32_t buffer_offset;
+  uint32_t data_size;
+  uint32_t volume;
+} riv_audio_ctl_desc;
 
-global riv_framebuffer_desc: type = @record{
-  height: uint32,
-  width: uint32,
-  fps: uint32,
-  pixel_format: riv_pixel_format,
-}
+typedef struct riv_mmio_header {
+  uint8_t magic[32];
+  uint32_t version;
+  uint32_t features;
+  uint64_t uuid;
+} riv_mmio_header;
 
-global riv_audio_ctl_desc: type = @record{
-  command: riv_audio_command,
-  flags: uint32, -- reserved
-  handle_id: uint64,
-  buffer_offset: uint32, -- reserved
-  data_size: uint32,
-  volume: uint32,
-}
+typedef struct riv_mmio_driver {
+  riv_mmio_header header;
+  uint64_t frame;
+  riv_audio_ctl_desc audio_ctl;
+  riv_framebuffer_desc framebuffer_desc;
+  bool tracked_keys[128];
+  uint32_t palette[256];
+} riv_mmio_driver;
 
-global riv_mmio_header: type = @record{
-  magic: [32]byte,
-  version: uint32,
-  features: uint32, -- reserved
-  uuid: uint64,
-}
+typedef struct riv_mmio_device {
+  riv_mmio_header header;
+  uint32_t key_event_count;
+  uint8_t key_events[128];
+} riv_mmio_device;
 
--- Only driver writes, only device reads.
-global riv_mmio_driver: type = @record{
-  header: riv_mmio_header,
-  frame: uint64,
-  audio_ctl: riv_audio_ctl_desc,
-  framebuffer_desc: riv_framebuffer_desc,
-  tracked_keys: [128]boolean,
-  palette: [256]uint32,
-}
+////////////////////////////////////////////////////////////////////////////////
+// Structures used only in the driver
 
--- Only device writes, only driver reads.
-global riv_mmio_device: type = @record{
-  header: riv_mmio_header,
-  key_event_count: uint32,
-  key_events: [128]uint8,
-}
+typedef struct riv_prng {
+  uint64_t state[4];
+} riv_prng;
 
---[[
-global riv_input_log_format: type <using> = @enum(uint8) {
-  RIV_INPUTLOG_INVALID = 0,
-  RIV_INPUTLOG_UNPACKED_KEYS = 1,
-}
+typedef struct riv_key_state {
+  bool press;           // Becomes trues when the key is pressed, becomes false only when it is released
+  bool down;            // True only in the frame the key is pressed
+  bool up;              // True only in the frame the key is released
+  uint64_t down_frame;  // Last frame the key was pressed
+  uint64_t up_frame;    // Last frame the key was released
+} riv_key_state;
 
-global riv_log_key: type <packed> = @record{
-  key_code: uint8,
-  down: boolean,
-  frame: uint64
-}
+typedef struct riv_context {
+  // private fields
+  riv_mmio_driver* mmio_driver;
+  riv_mmio_device* mmio_device;
+  int32_t yield_fd;
+  // public read-only fields
+  riv_key_state keys[128];
+  uint64_t frame;
+  // public read/write fields
+  bool running;
+  uint8_t* framebuffer;
+  uint8_t* audiobuffer;
+  riv_framebuffer_desc* framebuffer_desc;
+  bool* tracked_keys;
+  uint32_t* palette;
+} riv_context;
 
-global riv_replay: type = @record{
-  format: uint8,
-  version: uint8,
-  flags: uint16,
-  frames: uint64,
-  events: span(riv_log_key)
-}
-]]
+typedef void (*riv_context_callback)(riv_context*);
 
---------------------------------------------------------------------------------
--- Structures for driver
+typedef struct riv_run_desc {
+  riv_context_callback init_cb;
+  riv_context_callback cleanup_cb;
+  riv_context_callback frame_cb;
+  riv_framebuffer_desc framebuffer_desc;
+} riv_run_desc;
 
--- Pseudo random number generator.
-global riv_prng: type = @record{
-  state: [4]uint64
-}
+////////////////////////////////////////////////////////////////////////////////
+// API
 
-global riv_key_state: type = @record{
-  press: boolean,     -- Becomes trues when the key is pressed, becomes false only when it is released
-  down: boolean,      -- True only in the frame the key is pressed
-  up: boolean,        -- True only in the frame the key is released
-  down_frame: uint64, -- Last frame the key was pressed
-  up_frame: uint64,   -- Last frame the key was released
-}
+// utils
+uint64_t riv_version(void);
+uint64_t riv_rdcycle(void);
+int32_t riv_printf(char* format, ...);
 
-global riv_context: type = @record{
-  -- internal
-  mmio_driver: *riv_mmio_driver,
-  mmio_device: *riv_mmio_device,
-  yield_fd: int32,
-  -- public read-only fields
-  keys: [128]riv_key_state,
-  frame: uint64,
-  -- public read/write fields
-  running: boolean,
-  framebuffer: *[RIV_MEMSIZE_FRAMEBUFFER]byte,
-  audiobuffer: *[RIV_MEMSIZE_AUDIOBUFFER]byte,
-  framebuffer_desc: *riv_framebuffer_desc,
-  tracked_keys: *[128]boolean,
-  palette: *[256]uint32,
-}
+// context
+void riv_init(riv_context* self);
+void riv_uninit(riv_context* self);
+void riv_ctl(riv_context* self, riv_control_reason reason);
+void riv_present(riv_context* self);
+void riv_loop(riv_context* self, riv_context_callback frame_cb);
+uint64_t riv_sound_play_from_memory(riv_context* self, riv_span_uint8 data, uint32_t vol);
+void riv_sound_stop(riv_context* self, uint64_t sound_id);
+void riv_run(riv_run_desc* run_desc);
+void riv_stdrun(riv_context_callback frame_cb);
 
-global riv_context_callback: type = @function(*riv_context)
+// prng
+uint64_t riv_rand(riv_prng* self);
+uint64_t riv_rand_uint(riv_prng* self, uint64_t high);
+int64_t riv_rand_int(riv_prng* self, int64_t low, int64_t high);
+double riv_rand_float(riv_prng* self);
+void riv_seed(riv_prng* self, uint64_t a, uint64_t b);
 
-global riv_run_desc: type = @record{
-  init_cb: riv_context_callback,
-  cleanup_cb: riv_context_callback,
-  frame_cb: riv_context_callback,
-  framebuffer_desc: riv_framebuffer_desc,
-}
+#endif
