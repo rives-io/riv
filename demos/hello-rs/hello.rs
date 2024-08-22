0984b6defa4bb1226062e3477131745a77e4b9b4
ViniciosLugli <vinicioslugli@gmail.com>
2024-08-22 01:01:43 -0300

    feat(demo): :sparkles: Updated hello-rs to use bindgen with makefile setup

--- demos/hello-rs/hello.rs
+++ demos/hello-rs/hello.rs
@@ -1,10 +1,36 @@
-extern "C" {
-    pub fn riv_present() -> bool;
-    pub fn riv_clear(col: u32) -> bool;
-}
+use std::ffi::CString;
+#[allow(
+    non_snake_case,
+    non_camel_case_types,
+    non_upper_case_globals,
+    dead_code,
+    unused_imports
+)]
+mod riv;
+use riv::*;
 
 fn main() {
-    while unsafe { riv_present() } {
-        unsafe { riv_clear(1); }
+    unsafe {
+        let image_path = CString::new("assets/ferris.png").expect("CString::new failed");
+        let text = CString::new("hello rust!").expect("CString::new failed");
+
+        let img_id = riv_make_image(image_path.as_ptr(), 0);
+
+        loop {
+            riv_clear(0);
+            riv_draw_text(
+                text.as_ptr(),
+                riv_spritesheet_id::RIV_SPRITESHEET_FONT_5X7 as u64,
+                riv_align::RIV_CENTER,
+                128,
+                30,
+                2,
+                riv_color_id::RIV_COLOR_WHITE as i64,
+            );
+            riv_draw_image_rect(img_id, 8, 10, 256, 256, 0, 0, 1, 1);
+            if !riv_present() {
+                break;
+            }
+        }
     }
 }
