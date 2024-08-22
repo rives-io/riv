435f1795c5dd76e7b9f5b55dbcd54cf170321c84
Eduardo Bart <edub4rt@gmail.com>
2024-08-22 12:07:53 -0300

    Make Rust example generate smaller binaries

--- demos/hello-rs/hello.rs
+++ demos/hello-rs/hello.rs
@@ -1,25 +1,16 @@
-use std::ffi::CString;
-#[allow(
-    non_snake_case,
-    non_camel_case_types,
-    non_upper_case_globals,
-    dead_code,
-    unused_imports
-)]
+#![no_main] // By not using standard Rust main entrypoint greatly reduces binary size.
+#[allow(non_camel_case_types,dead_code,unused_imports)]
 mod riv;
 use riv::*;
 
-fn main() {
+#[no_mangle]
+unsafe extern fn main() {
     unsafe {
-        let image_path = CString::new("assets/ferris.png").expect("CString::new failed");
-        let text = CString::new("hello rust!").expect("CString::new failed");
-
-        let img_id = riv_make_image(image_path.as_ptr(), 0);
-
+        let img_id = riv_make_image(c"assets/ferris.png".as_ptr(), 0);
         loop {
-            riv_clear(0);
+            riv_clear(riv_color_id::RIV_COLOR_BLACK as u32);
             riv_draw_text(
-                text.as_ptr(),
+                c"hello rust!".as_ptr(),
                 riv_spritesheet_id::RIV_SPRITESHEET_FONT_5X7 as u64,
                 riv_align::RIV_CENTER,
                 128,
